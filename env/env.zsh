bdf792c3ca75227fee7cf7ddc3add2984df7632a
Fernando Rivas <fernando.rivas@ponderalab.com>
2018-03-14 20:07:17 -0600

    Fix path and add path for osx

--- env/env.zsh
+++ env/env.zsh
@@ -6,17 +6,22 @@
 WWW_HOME='https://www.duckduckgo.com'
 export WWW_HOME
 
-export LESSOPEN="| /usr/bin/src-hilite-lesspipe.sh %s"
 
-#
 # OS Specific Env Variables
 #
 
 if [[ $CURRENT_OS == 'OS X' ]]; then
     # OS X Env Variables
     export ZSH_CACHE_DIR=$HOME/.zsh
+    # less support for highlighting code
+    export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
+
 elif [[ $CURRENT_OS == 'Linux' ]]; then
     # Linux Env Variables
+    # less support for highlighting code
+    # taken from here: https://superuser.com/questions/71588/how-to-syntax-highlight-via-less
+    export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+#
 elif [[ $CURRENT_OS == 'Cygwin' ]]; then
     # Cygwin Env Variables
 fi
