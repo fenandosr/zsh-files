bff365fc3cca57581318ca2a557545a36b7b8ac1
fernando <fenandos@hotmail.com>
2020-11-10 16:27:14 -0600

    Updates, mainly paths...

--- completion/general.zsh
+++ completion/general.zsh
@@ -1,4 +1,16 @@
 # General completion patterns
 
+autoload -Uz compinit
+compinit
+
 # teamocil
 compctl -g '~/.teamocil/*(:t:r)' teamocil
+
+# compdef toggl
+_toggl() {
+  eval $(env COMMANDLINE="${words[1,$CURRENT]}" _TOGGL_COMPLETE=complete-zsh  toggl)
+}
+if [[ "$(basename -- ${(%):-%x})" != "_toggl" ]]; then
+  compdef _toggl toggl
+fi
+
--- path/paths.zsh
+++ path/paths.zsh
@@ -23,45 +23,36 @@ if [[ $CURRENT_OS == 'OS X' ]]; then
     export PATH=~/.rbenv/shims:$PATH
 
     #
-    # Normal Paths
+    # Common paths
     #
-
+    export PATH="/usr/local/opt/libxml2/bin:/usr/local/opt/python/libexec/bin:/usr/local/sbin:$PATH"
     # MIT-Scheme
     export MITSCHEME_LIBRARY_PATH=/usr/local/lib/mit-scheme-c/
     #  MacPorts
     export PATH=$PATH:/opt/local/bin
-    #  Add the bin/Mac directory
-    export PATH=$PATH:~/bin/Mac
     # Add all npm packages
     export PATH=$PATH:/usr/local/share/npm/bin
     # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
     export PATH="$PATH:$HOME/.rvm/bin"
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-    # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-    export PATH="$PATH:$HOME/.rvm/bin"
-    # Others
-    export PATH="/usr/local/opt/libxml2/bin:/usr/local/opt/python/libexec/bin:/usr/local/sbin:$PATH"
-
 
 elif [[ $CURRENT_OS == 'Linux' ]]; then
-
     # pyenv
     export PATH=$PATH:$HOME/.pyenv/bin
 
-
-    # Go's Path
+    # Go
     export PATH=$PATH:/usr/local/go/bin
     export PATH=$PATH:$HOME/go/bin
     export PATH=$PATH:$HOME/.local/bin
 
-    # Path for selenium webdriver
+    # Selenium webdriver
     export PATH=$PATH:/opt/webdriver
 
-    # Path for snaps
+    # Snaps
     export PATH=$PATH:/snap/bin
 
-    NODEVERSION=v10.16.0
-    NODEDISTRO=linux-x64
+    # Linuxbrew
+    export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 
     #
     # User Specific Aliases
@@ -70,14 +61,6 @@ elif [[ $CURRENT_OS == 'Linux' ]]; then
         # Home Linux
     elif [[ $USER == 'rogue' ]]; then
         # Work Linux
-
-        # Android SDK Tools
-        #export PATH=$PATH:~/Programming/SDKs/tools
-        # Android SDK Platform Tools
-        #export PATH=$PATH:~/Programming/SDKs/platform-tools
-        # Jars
-        #export PATH=$PATH:~/jars
-        export PATH=/usr/local/lib/nodejs/node-$NODEVERSION-$NODEDISTRO/bin:$PATH
     fi
 elif [[ $CURRENT_OS == 'Cygwin' ]]; then
     # nothing yet
