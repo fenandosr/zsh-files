1fb96f250f72e679329844db259edb2b4ce997ec
Fernando Rivas <fenandos@hotmail.com>
2021-02-06 15:54:45 -0600

    Java and Android dev paths

--- path/paths.zsh
+++ path/paths.zsh
@@ -37,6 +37,10 @@ if [[ $CURRENT_OS == 'OS X' ]]; then
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 elif [[ $CURRENT_OS == 'Linux' ]]; then
+    # JAVA_HOME
+    #export JAVA_HOME=/usr/lib/jvm/openjdk-11
+    export JAVA_HOME=/opt/jdk/jdk1.8.0_281
+
     # pyenv
     export PATH=$PATH:$HOME/.pyenv/bin
 
@@ -53,6 +57,10 @@ elif [[ $CURRENT_OS == 'Linux' ]]; then
 
     # Flutter
     export PATH=$PATH:$HOME/Apps/flutter/bin
+
+    # Android SDK
+    export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+    export PATH=$PATH:$HOME/Android/Sdk/platform-tools:$HOME/Android/Sdk/tools
 
     #
     # User Specific Aliases
