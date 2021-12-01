8e795880c2e26d99ee3367ec290ba90b32f7d141
Fernando Rivas <fernando@bit-a-bit.info>
2021-11-30 23:12:08 -0600

    NPM & SDKMAN paths

--- path/paths.zsh
+++ path/paths.zsh
@@ -29,17 +29,26 @@ if [[ $CURRENT_OS == 'OS X' ]]; then
 elif [[ $CURRENT_OS == 'Linux' ]]; then
     # pyenv
     export PATH=$PATH:$HOME/.pyenv/bin
-
     # Go
     export PATH=$PATH:/usr/local/go/bin
     export PATH=$PATH:$HOME/go/bin
     export PATH=$PATH:$HOME/.local/bin
     export GOPATH=$HOME/go
+    # Node Version Manager
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    # SDKMAN
+    if [ -d "$HOME/.sdkman" ]
+    then
+        export SDKMAN_DIR="$HOME/.sdkman"
+        [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+    fi
+
 
     #
     # User Specific Aliases
     #
-    if [[ $USER == 'su' ]]; then
+    if [[ $USER == 'none' ]]; then
         # none
     fi
 elif [[ $CURRENT_OS == 'Cygwin' ]]; then
