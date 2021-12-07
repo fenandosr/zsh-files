156cc457e31ecae6dd758fe4e1ec945ec12073f4
Fernando Rivas <fernando@bit-a-bit.info>
2021-12-06 23:43:15 -0600

    peco-history on ctrl+r

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -15,8 +15,6 @@ if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
   zle -N zle-line-finish
 fi
 
-bindkey '^r' peco-select-history                      # peco history
-
 bindkey -e                                            # Use emacs key bindings
 
 bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
@@ -60,6 +58,8 @@ else
   bindkey "^[3;5~" delete-char
   bindkey "\e[3~" delete-char
 fi
+
+bindkey '^r' peco-select-history                      # peco history
 
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
