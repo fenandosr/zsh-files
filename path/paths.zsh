fdf4c2009345570059fdb4cb07f379401ef723b0
Fernando Rivas <fernando@bit-a-bit.info>
2022-11-23 13:31:40 -0600

    Add paths.

--- path/paths.zsh
+++ path/paths.zsh
@@ -16,15 +16,22 @@ if [[ $CURRENT_OS == 'OS X' ]]; then
     # Prefixed Paths
     #
 
-    # Prefix /usr/local/bin for brew
-    export PATH=/usr/local/bin:$PATH
+    # Prefix for brew
+    export PATH=/opt/homebrew/bin:$PATH
 
+    # Node Version Manager
+    export NVM_DIR="${HOME}/.nvm"
+    source $(brew --prefix nvm)/nvm.sh
+
+    # OpenJDK
+    export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+    export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
     #
     # Common paths
     #
-    export PATH="/usr/local/opt/libxml2/bin:/usr/local/sbin:$PATH"
+    #export PATH="/usr/local/opt/libxml2/bin:/usr/local/sbin:$PATH"
     #  MacPorts
-    export PATH=$PATH:/opt/local/bin
+    #export PATH=$PATH:/opt/local/bin
 
 elif [[ $CURRENT_OS == 'Linux' ]]; then
     # pyenv
@@ -34,8 +41,10 @@ elif [[ $CURRENT_OS == 'Linux' ]]; then
     export PATH=$PATH:$HOME/go/bin
     export PATH=$PATH:$HOME/.local/bin
     export GOPATH=$HOME/go
+ 
     # Node Version Manager
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     # SDKMAN
     if [ -d "$HOME/.sdkman" ]
