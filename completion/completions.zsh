663853e82447134aa9ec932c14795c81f4c202c4
Fernando Rivas <fernando@bit-a-bit.info>
2024-04-15 18:02:42 -0600

    No completions

--- completion/completions.zsh
+++ completion/completions.zsh
@@ -1,11 +1,2 @@
-#
-# General
-#
+# custom additional completions
 
-#compdef toggl
-_toggl() {
-  eval $(env COMMANDLINE="${words[1,$CURRENT]}" _TOGGL_COMPLETE=complete-zsh  toggl)
-}
-if [[ "$(basename -- ${(%):-%x})" != "_toggl" ]]; then
-  compdef _toggl toggl
-fi
